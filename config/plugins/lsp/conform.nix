ad17bd995adddce099d0d98387c3c7aed509f640
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2024-10-23 22:48:08 +0300

    enable the yaml formatter
    By default the yaml formatter was disabled.

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -164,9 +164,9 @@
           bicep = {
             command = "${lib.getExe pkgs.bicep}";
           };
-          #yamlfmt = {
-          #  command = "${lib.getExe pkgs.yamlfmt}";
-          #};
+          yamlfmt = {
+            command = "${lib.getExe pkgs.yamlfmt}";
+          };
         };
       };
     };
